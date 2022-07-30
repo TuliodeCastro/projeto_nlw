550623c8252cd0066744ad422cfa567410745804
Túlio <tulioleandrodecastro@hotmail.com>
2022-07-30 14:04:30 -0300

    update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
 import { MessagesController } from "./controllers/MessagesController";
 
-
 const routes = Router();
 
 const settingsController = new SettingsController();
@@ -11,6 +10,8 @@ const usersController = new UsersController();
 const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
+routes.get("/settings/:username", settingsController.findByUsername);
+routes.put("/settings/:username", settingsController.update);
 
 routes.post("/users", usersController.create);
 
