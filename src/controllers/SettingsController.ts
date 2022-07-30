723f171069f75face0f0c8de113538954bf74b0d
Túlio <tulioleandrodecastro@hotmail.com>
2022-07-30 14:09:52 -0300

    updated controllers

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -5,7 +5,8 @@ class SettingsController {
     async create(request: Request, response: Response){
         const { chat, username } = request.body;
 
-        const settingsService = new SettingsService;
+        // Se parar de funcionar, tirar o () de SettingsService
+        const settingsService = new SettingsService();
 
         try {
             const settings = await settingsService.create({ chat, username });
@@ -14,8 +15,28 @@ class SettingsController {
         } catch(err) {
             return response.status(400).json({
                 message: err.message,
-            })
+            });
         }
+    }
+
+    async findByUsername(request: Request, response: Response) {
+      const { username } = request.params;
+
+      const settingsService = new SettingsService();
+
+      const settings = await settingsService.findByUsername(username);
+
+      return response.json(settings);
+    }
+
+    async update(request: Request, response: Response) {
+      const { username } = request.params;
+      const { chat } = request.body;
+
+      const settingsService = new SettingsService();
+
+      const settings = await settingsService.update(username, chat);
+      return response.json(settings);
     }
 }
 
